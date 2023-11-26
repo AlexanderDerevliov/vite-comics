b05d06b08bdb709c6302692435d697e8dedc581f
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-26 08:50:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppContent from './components/AppContent.vue';
+import AppLogos from './components/AppLogos.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppContent,
+    AppLogos,
+    AppFooter,
+    
+},
+  data (){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+  
+  
+ 
+ <AppHeader />
+ <AppContent />
+ <AppLogos />
+ <AppFooter />
+
+</template>
+
+<style>
+
+
+
+</style>
